0b3fa9f259c6702d12ee8d2687ec3e3698eefe21
Yayoi <95954729+84po12pu@users.noreply.github.com>
2022-02-27 00:26:57 +0900

    変更

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 'use strict';
 
-//iphone用100vh(jquery)
-    // $('.top').css('height',$(window).height());
+// animate
+$('.leftTrigger').on('inview', function(event, isInView) {
+    if (isInView) {//表示領域に入った時
+      $(this).addClass('animate__animated animate__fadeInLeft');//クラス名が付与
+    }
+  });
 
+  $('.rightTrigger').on('inview', function(event, isInView) {
+    if (isInView) {//表示領域に入った時
+      $(this).addClass('animate__animated animate__fadeInRight');//クラス名が付与
+    }
+  });
